8d9bfe309d32c94b74b47ab96466f6dc554406bc
Femarko <fenstermark7@gmail.com>
2024-01-10 03:29:12 +0100

    DATABASES modified in settings.py: postgres -> sqlite3

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': name,
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-        'USER': user,
-        'PASSWORD': password,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
